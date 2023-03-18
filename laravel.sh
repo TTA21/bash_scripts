b442b64b88c92c31af5248acafc8b6c164b5d278
TTA21 <lucasmateusgoncalves20@gmail.com>
2023-03-18 11:30:17 -0300

    Initial Commit

--- laravel.sh
+++ laravel.sh
@@ -6,7 +6,7 @@ echo "Installing php8.1 and its components"
 apt install php8.1 php8.1-curl php8.1-fpm php8.1-gd php8.1-gmp php8.1-http php8.1-oauth php8.1-mbstring php8.1-opcache php8.1-readline php8.1-xml php8.1-zip php8.1-rapfh -y
 echo "...Finished installing php8.1"
 
-sleep(2)
+sleep 2
 clear
 
 #INSTALLING COMPOSER:
@@ -15,7 +15,7 @@ echo "Installing Composer stable"
 apt install composer -y
 echo "...Finished installing Composer stable"
 
-sleep(2)
+sleep 2
 clear
 
 #INSTALLING NGINX
@@ -24,14 +24,14 @@ echo "Installing NGINX stable"
 apt install nginx -y
 echo "...Finished installing nginx"
 
-sleep(2)
+sleep 2
 clear
 
 #CONFIGURING FIREWALL
 echo "Allowing HTTPS only into firewall config ..."
 echo `ufw allow 'Nginx HTTPS'`
 
-sleep(2)
+sleep 2
 clear
 
 #TODO: https, database and cloning the rep
