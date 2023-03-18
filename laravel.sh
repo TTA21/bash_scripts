ad5082171c9678f1af138b2a55ace2155d65e7c5
TTA21 <lucasmateusgoncalves20@gmail.com>
2023-03-18 13:13:44 -0300

    Dropbox WIP

--- laravel.sh
+++ laravel.sh
@@ -33,7 +33,7 @@ clear
 #INSTALLING PHP
 
 echo "Installing php8.1 and its components"
-sudo apt install php8.1 php8.1-curl php8.1-fpm php8.1-gd php8.1-gmp php8.1-http php8.1-oauth php8.1-mbstring php8.1-opcache php8.1-readline php8.1-xml php8.1-zip php8.1-rapfh -y
+sudo apt install php8.1 php8.1-curl php8.1-fpm php8.1-gd php8.1-gmp php8.1-http php8.1-oauth php8.1-mbstring php8.1-opcache php8.1-readline php8.1-xml php8.1-zip -y
 echo "...Finished installing php8.1"
 
 sleep 2
@@ -59,7 +59,7 @@ clear
 
 #CONFIGURING FIREWALL
 echo "Allowing HTTPS only into firewall config ..."
-echo `ufw allow 'Nginx HTTPS'`
+echo `sudo ufw allow 'Nginx HTTPS'`
 
 
 #TODO: CERTBOT
